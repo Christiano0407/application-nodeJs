f7fa93e6f07f555c1653b94195017983b1a68533
Christiano0407 <placebo0704@gmail.com>
2023-01-31 08:45:03 -0600

    server && application node part 02

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-//** ================================= First Application With Node Js && Vanilla Javascript ============================== Front-end && Backend ============================== == */
+//** ===== SERVER ============================ First Application With Node Js && Vanilla Javascript ============================== Front-end && Backend ============================== == */
+//** === Express === (Type: module) */
+//const express =  require(`express`);
+import express from 'express';
+//**! App === */
+const app = express();
+//**! PORT === */
+const port = process.env.port || 3000;
+
+//**! Routing === */
+app.get(`/`, (req, res) => {
+  res.send(`Hello World!!`);
+});
+
+//**! Listen === */
+app.listen(port, () => {
+  console.log(`Server is running in Port: http://localhost:${port}`);
+});
